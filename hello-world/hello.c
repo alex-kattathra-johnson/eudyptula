94b9bcd783f3f0477c48f8ba0f761708148708f5
Alex Johnson <me@alex-johnson.net>
2016-03-14 19:03:05 -0400

    nitpick

--- hello-world/hello.c
+++ hello-world/hello.c
@@ -1,6 +1,6 @@
-#include <linux/module.h> 
-#include <linux/kernel.h>  
-#include <linux/init.h>      
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
 
 static int __init hello_world(void)
 {
